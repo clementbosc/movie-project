cf5265a1f1fb5912f829a403cba5abc67fd2054f
Clément Bosc <clement.bosc@outlook.com>
2017-02-26 21:24:48 +0100

    Connexion, utilisateur, acceuil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  @@language = "fr-FR"
+  @@api_key = "32c2115b63d2b870f0f58d85da6a8183"
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220170606) do
+ActiveRecord::Schema.define(version: 20170226201157) do
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.date     "release_date"
-    t.text     "overview"
-    t.string   "budget"
-    t.string   "homepage"
-    t.string   "original_language"
-    t.string   "original_title"
-    t.float    "popularity"
-    t.string   "revenue"
-    t.string   "status"
-    t.string   "tagline"
-    t.string   "backdrop_path"
-    t.string   "poster_path"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
