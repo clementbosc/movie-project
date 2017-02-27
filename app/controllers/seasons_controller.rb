07d30202b862a95b5a30329a593cc6891fb2545d
Clément Bosc <clement.bosc@outlook.com>
2017-02-27 17:15:39 +0100

    Ajout pages episodes, seasons, users

---
+++ app/controllers/seasons_controller.rb
@@ -0,0 +1,6 @@
+class SeasonsController < ApplicationController
+  def show
+    buffer = open("https://api.themoviedb.org/3/tv/"+params[:show]+"/season/"+params[:season]+"?api_key="+@@api_key+"&language="+@@language).read
+    @season = JSON.parse(buffer)
+  end
+end
