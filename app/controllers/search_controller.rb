7d7185e9efc0b4583e8d3863e0dbb8e647216ab9
Clément Bosc <clement.bosc@outlook.com>
2017-02-28 10:56:16 +0100

    Correction bug recherche

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def search
-    buffer = open('https://api.themoviedb.org/3/search/multi?query='+params[:query]+'&api_key='+@@api_key+'&language='+@@language).read
+    buffer = open('https://api.themoviedb.org/3/search/multi?query='+URI.encode(params[:query])+'&api_key='+@@api_key+'&language='+@@language).read
     @results = JSON.parse(buffer)['results']
   end
 end
